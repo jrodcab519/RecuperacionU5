0f63281cc4c672c4fcaadff8c2915c96a110de3c
jrodcab519 <jrodcab519@g.educaand.es>
2024-06-07 14:55:03 +0200

    Version inicial

--- src/Ut6_3.java
+++ src/Ut6_3.java
@@ -14,7 +14,7 @@ public class Ut6_3 {
             if(palabra.charAt(0) == '0' || palabra.charAt(0) == '1' || palabra.charAt(0) == '2' || palabra.charAt(0) == '3' || palabra.charAt(0) == '4' || palabra.charAt(0) == '5' || palabra.charAt(0) == '6' || palabra.charAt(0) == '7' || palabra.charAt(0) == '8' || palabra.charAt(0) == '9' ){
                 System.out.println("DÍGITOS");
             }
-            if(palabra.contains("A") || palabra.contains("E") || palabra.contains("I") || palabra.contains("O") || palabra.contains("U") && !palabra.equals("FIN")) {
+            if(palabra.contains("A") || palabra.contains("E") || palabra.contains("I") || palabra.contains("O") || palabra.contains("U")) {
                 System.out.println("VOCAL");
             }
             if(palabra.contains("II") || palabra.contains("III")){
