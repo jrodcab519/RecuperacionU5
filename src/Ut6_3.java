67236197146733cce8a85e764270a2277a521518
jrodcab519 <jrodcab519@g.educaand.es>
2024-06-07 14:54:52 +0200

    Version inicial

--- src/Ut6_3.java
+++ src/Ut6_3.java
@@ -1,4 +1,4 @@
-import java.util.Formatter;
+
 import java.util.Scanner;
 
 public class Ut6_3 {
@@ -14,13 +14,13 @@ public class Ut6_3 {
             if(palabra.charAt(0) == '0' || palabra.charAt(0) == '1' || palabra.charAt(0) == '2' || palabra.charAt(0) == '3' || palabra.charAt(0) == '4' || palabra.charAt(0) == '5' || palabra.charAt(0) == '6' || palabra.charAt(0) == '7' || palabra.charAt(0) == '8' || palabra.charAt(0) == '9' ){
                 System.out.println("DÍGITOS");
             }
-            if(palabra.contains("A") || palabra.contains("E") || palabra.contains("I") || palabra.contains("O") || palabra.contains("U")) {
+            if(palabra.contains("A") || palabra.contains("E") || palabra.contains("I") || palabra.contains("O") || palabra.contains("U") && !palabra.equals("FIN")) {
                 System.out.println("VOCAL");
             }
             if(palabra.contains("II") || palabra.contains("III")){
                 System.out.println("ROMANO");
             }
-            CharSequence Formatter = "A1a";
+            CharSequence Formatter = "I2a";
             if(palabra.contains(Formatter)){
                 System.out.println("TRES");
             }
